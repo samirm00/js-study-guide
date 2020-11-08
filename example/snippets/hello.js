1786acfd66f89d72558226517a43f9e5ff7968ea
Samir <sammoham.be@gmail.com>
2020-11-08 02:30:29 +0100

    Add hello world to the home page

---
+++ example/snippets/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Hello World!");
+console.log(
+  "Welcome to Our Javascript tutorial , JavaScript is the most popular programing languages ,it is the language of the Web"
+);
