37dfb1382805d671141767fa2ec39edfc4a0871f
Houcinos <houcinos.dhieb@gmail.com>
2020-11-11 16:53:03 +0100

    add the loops files

---
+++ exersises/While_exp1.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+}
---
+++ exersises/While_exp2.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Infinite loops are bad!
+while (true) {
+  console.log("Hello, world!");
+}
